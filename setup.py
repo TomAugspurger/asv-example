b9ee2a07a2aaeff5b9b3382ccddfe225c0d27970
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-05-31 13:16:39 -0500

    other repo

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name='asvexample',
-    version='1.2.0',
-    description='A sample Python project',
-    long_description='',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-)
