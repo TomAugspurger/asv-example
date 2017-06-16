1c941117f108d4ba4a407d1695c065721dd438f5
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-06-16 14:08:14 -0500

    quicker

--- benchmarks/array.py
+++ benchmarks/array.py
@@ -4,11 +4,10 @@ import timeit
 
 class Main(object):
 
-    goal_time = 2.0
     timer = timeit.default_timer
 
     def setup(self):
         pass
 
     def time_it(self):
-        time.sleep(1.5)
+        time.sleep(.001)
